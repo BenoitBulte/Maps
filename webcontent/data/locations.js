36e916f7b14702f5eb5e5fd8dc1740bd708e03bf
Benoit BULTE <bbulte@laposte.net>
2016-10-20 08:06:01 +0000

    First

---
+++ webcontent/data/locations.js
@@ -0,0 +1,459 @@
+var LocsA = [
+        {
+	lat: 37.16,
+	lon: -77.96,
+	title: 'AWS Virginie',
+	html: [
+	    '<h3>AWS Virginie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },
+        {
+	lat: 43.7,
+	lon: -120.45,
+	title: 'AWS Oregon',
+	html: [
+	    '<h3>AWS Oregon</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },
+    {
+	lat: 36.67,
+	lon: -119.31,
+	title: 'AWS Californie',
+	html: [
+	    '<h3>AWS Californie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },
+    {
+	lat: 36.98,
+	lon: -96.06,
+	title: 'AWS Etats-Unis',
+	html: [
+	    '<h3>AWS Etats-Unis</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },
+	{
+	lat: -14.13,
+	lon: -51.82,
+	title: 'AWS Bresil',
+	html: [
+	    '<h3>AWS Bresil</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },		
+	{
+	lat: 53.31,
+	lon: -8.14,
+	title: 'AWS Irlande',
+	html: [
+	    '<h3>AWS Irlande</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },
+    {
+	lat: 51.06,
+	lon: 10.35,
+	title: 'AWS Allemagne',
+	html: [
+	    '<h3>AWS Allemagne</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },
+	{
+	lat: 1.25,
+	lon: 103.71,
+	title: 'AWS Singapour',
+	html: [
+	    '<h3>AWS Singapour</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },		
+	{
+	lat: 35.8,
+	lon: 127.66,
+	title: 'AWS Coree du Sud',
+	html: [
+	    '<h3>AWS Coree du Sud</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },	
+	{
+	lat: 36.1,
+	lon: 138.15,
+	title: 'AWS Japon',
+	html: [
+	    '<h3>AWS Japon</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },		
+	{
+	lat: 35.76,
+	lon: 104.09,
+	title: 'AWS Chine',
+	html: [
+	    '<h3>AWS Chine</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    },	
+	{
+	lat: -25.17,
+	lon: 132.09,
+	title: 'AWS Australie',
+	html: [
+	    '<h3>AWS Australie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerA.png',
+	zoom: 4
+    }
+];
+
+var LocsG = [
+        {
+	lat: 50.4,
+	lon: 4.56,
+	title: 'Google Belgique',
+	html: [
+	    '<h3>Google Belgique</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerG.png',
+	zoom: 4
+    },
+        {
+	lat: 23.59,
+	lon: 121.06,
+	title: 'Google Taiwan',
+	html: [
+	    '<h3>Google Taiwan</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerG.png',
+	zoom: 4
+    },
+    {
+	lat: 41.75,
+	lon: -93.22,
+	title: 'Google Iowa',
+	html: [
+	    '<h3>Google Iowa</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerG.png',
+	zoom: 4
+    },
+    {
+	lat: 33.73,
+	lon: -81.26,
+	title: 'Google Caroline du Sud',
+	html: [
+	    '<h3>Google Caroline du Sud</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerG.png',
+	zoom: 4
+    }
+];
+
+var LocsI = [
+    {
+	lat: 37.18,
+	lon: -96.26,
+	title: 'BlueMix Etats-Unis',
+	html: [
+	    '<h3>BlueMix Etats-Unis</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 55.42,
+	lon: -3.8,
+	title: 'BlueMix Royaume-Uni',
+	html: [
+	    '<h3>BlueMix Royaume-Uni</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: -25.37,
+	lon: 132.29,
+	title: 'BlueMix Australie',
+	html: [
+	    '<h3>BlueMix Australie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 56.23,
+	lon: -106.44,
+	title: 'BlueMix Canada',
+	html: [
+	    '<h3>BlueMix Canada</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: -14.33,
+	lon: -52.02,
+	title: 'BlueMix Bresil',
+	html: [
+	    '<h3>BlueMix Bresil</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 23.73,
+	lon: -102.65,
+	title: 'BlueMix Mexique',
+	html: [
+	    '<h3>BlueMix Mexique</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 46.32,
+	lon: 2.31,
+	title: 'BlueMix France',
+	html: [
+	    '<h3>BlueMix France</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 51.26,
+	lon: 10.55,
+	title: 'BlueMix Allemagne',
+	html: [
+	    '<h3>BlueMix Allemagne</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 41.97,
+	lon: 12.66,
+	title: 'BlueMix Italie',
+	html: [
+	    '<h3>BlueMix Italie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 52.3,
+	lon: 5.44,
+	title: 'BlueMix Pays-Bas',
+	html: [
+	    '<h3>BlueMix Pays-Bas</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 22.49,
+	lon: 114.2,
+	title: 'BlueMix Hong Kong',
+	html: [
+	    '<h3>BlueMix Hong Kong</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 20.69,
+	lon: 79.06,
+	title: 'BlueMix Inde',
+	html: [
+	    '<h3>BlueMix Inde</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 36.3,
+	lon: 128.35,
+	title: 'BlueMix Japon',
+	html: [
+	    '<h3>BlueMix Japon</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+    {
+	lat: 1.45,
+	lon: 103.91,
+	title: 'BlueMix Singapour',
+	html: [
+	    '<h3>BlueMix Singapour</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerI.png',
+	zoom: 4
+    },
+];
+
+var LocsM = [
+    {
+	lat: 41.85,
+	lon: -93.12,
+	title: 'Azure Iowa',
+	html: [
+	    '<h3>Azure Iowa</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },
+    {
+	lat: 37.26,
+	lon: -78.06,
+	title: 'Azure Virginie',
+	html: [
+	    '<h3>Azure Virginie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },
+    {
+	lat: 40.63,
+	lon: -89.39,
+	title: 'Azure Illinois',
+	html: [
+	    '<h3>Azure Illinois</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },
+    {
+	lat: 31.96,
+	lon: -99.9,
+	title: 'Azure Texas',
+	html: [
+	    '<h3>Azure Texas</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },
+    {
+	lat: 36.77,
+	lon: -119.41,
+	title: 'Azure Californie',
+	html: [
+	    '<h3>Azure Californie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },
+	{
+	lat: 53.41,
+	lon: -8.24,
+	title: 'Azure Irlande',
+	html: [
+	    '<h3>Azure Irlande</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },
+	{
+	lat: 52.2,
+	lon: 5.34,
+	title: 'Azure Pays-Bas',
+	html: [
+	    '<h3>Azure Pays-Bas</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },
+	{
+	lat: 22.39,
+	lon: 114.1,
+	title: 'Azure Hong Kong',
+	html: [
+	    '<h3>Azure Hong Kong</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },	
+	{
+	lat: 1.35,
+	lon: 103.81,
+	title: 'Azure Singapour',
+	html: [
+	    '<h3>Azure Singapour</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },		
+	{
+	lat: 36.2,
+	lon: 138.25,
+	title: 'Azure Japon',
+	html: [
+	    '<h3>Azure Japon</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },		
+	{
+	lat: -14.23,
+	lon: -51.92,
+	title: 'Azure Bresil',
+	html: [
+	    '<h3>Azure Bresil</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },		
+	{
+	lat: -25.27,
+	lon: 132.19,
+	title: 'Azure Australie',
+	html: [
+	    '<h3>Azure Australie</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },		
+	{
+	lat: 20.59,
+	lon: 78.96,
+	title: 'Azure Inde',
+	html: [
+	    '<h3>Azure Inde</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },	
+	{
+	lat: 35.86,
+	lon: 104.19,
+	title: 'Azure Chine',
+	html: [
+	    '<h3>Azure Chine</h3>',
+	].join(''),
+	icon: 'http://maps.google.com/mapfiles/markerM.png',
+	zoom: 4
+    },	
+];
+
+
+
+
+
+
+
+
